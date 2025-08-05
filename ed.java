bf210e21634d066937bed4629e169a2778970cf4
maitri <maitriramoliya19@gmail.com>
2025-08-05 15:15:19 +0530

    new repo

---
+++ ed.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ed {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        
+        if(a%2==0)
+        {
+            System.out.println("Even number");
+            
+        }
+        else
+        {
+            System.out.println("Odd number");
+        
+        }
+        
+    }
+    
+}
